f8c29e8796d96ba5c046581646923125f034d7b4
nscodechris <christoffer.vdmab@gmail.com>
2022-07-13 10:46:45 +0200

    smhi_data_source_to_raw

--- weather_new.py
+++ weather_new.py
@@ -8,58 +8,54 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 
 
-
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 
 CURR_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
-api_key = "3ab30abfa2f442f4f2ecc32a856dd88a"
-
-city_name = "Stockholm" #you can ask for user input instead
-
-#Let's get the city's coordinates (lat and lon)
-
-
-url = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}'
+url = 'https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/16.158/lat/58.5812/data.json'
 # print(url)
 r = requests.get(url)
 jason_data = r.json()
 # print(type(jason_data))
-# print(jason_data)
+#print(jason_data)
 
+# dict = json.loads(r.text)
+# print(dict)
+# intressant = dict['timeSeries']
+#
+# print(intressant)
+
+# for i in range(len(intressant)):
+#     element = intressant[i]
+#     tidpunkt = element['validTime']
+#     # print(i, element, tidpunkt)
+#     # paus = input("Next?")
 
 if r.status_code == 200:  # If connection is successful (200: http ok)
     json_data = r.json()  # Get result in json
-
+    # print(jason_data)
     # Create a dictionary to represent the stored data
     # To view all accessible data see: https://openweathermap.org/current#current_JSON
-    weather_data = {
-        "weather": json_data["weather"][0],  # [0] because for some reason it's a single element list?
-        "main": json_data["main"],
-        "visibility": json_data["visibility"],
-        "wind": json_data["wind"],
-        "clouds": json_data["clouds"],
-        "date_time": datetime.fromtimestamp(json_data["dt"])
-    }
-    # Flattens dictionaries (normalize) because a dataframe can't contain nested dictionaries
-    # E.g. Internal dictionary {"weather": {"temp": 275, "max_temp": 289}}
-    # becomes {"weather.temp": 275, "weather.max_temp", 289}
-    weather_data = pd.json_normalize(weather_data)
-    df = pd.DataFrame(weather_data)
-    df["main.temp"] = df["main.temp"] - 273.15
-    df["main.feels_like"] = df["main.feels_like"] - 273.15
-    df["main.temp_min"] = df["main.temp_min"] - 273.15
-    df["main.temp_max"] = df["main.temp_max"] - 273.15
-    to_day = datetime.now().strftime("%Y_%m_%d_%I_%M_%S_%p")
-    print(df)
-    df2 = df[["date_time", "main.temp", "wind.speed", "main.pressure", "main.humidity"]]
-    print(df2)
+    weather_list = []
+    for i in range(len(json_data['timeSeries'])):
 
-    # print("today", to_day)
-    # df2.to_csv(CURR_DIR_PATH + "/data/" + str(to_day) + "_" + city_name + ".csv", index=False)
-    # df2.to_json(CURR_DIR_PATH + "/data/" + str(to_day) + "_" + city_name + ".json")
+        weather_data = {
+            "temperature": json_data['timeSeries'][i]["parameters"][10]['values'][0],
+            "air pressure": json_data['timeSeries'][i]["parameters"][11]['values'][0],
+            "precipitation": json_data['timeSeries'][i]["parameters"][3]['values'][0],
+            "date": json_data['timeSeries'][i]['validTime']
+        }
+        weather_list.append(weather_data)
+    # print(weather_list)
+    # print(type(weather_list))
+    df = pd.DataFrame(weather_list)
+    print(df)
+    to_day = datetime.now().strftime("%Y_%m_%d_%I_%M_%S_%p")
+
+    df.to_csv(CURR_DIR_PATH + "/data/" + str(to_day) + "_" + ".csv", index=False)
+    df.to_json(CURR_DIR_PATH + "/data/" + str(to_day) + "_" + ".json")
     # print(df)
 
 def plot_weather():
