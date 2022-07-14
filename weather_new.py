a905559e8b19e6d248ea662fc8493ca6d950faac
nscodechris <christoffer.vdmab@gmail.com>
2022-07-14 10:17:01 +0200

    2022-07-14-10:14

--- weather_new.py
+++ weather_new.py
@@ -29,94 +29,90 @@ dictr = response.json()
 list_values = []
 df_values = pd.json_normalize(list_values)
 
+temp_list = []
+air_pressure_list = []
+precipitation_list = []
+
+# pcat
+new_temp_list = []
+new_air_pressure_list = []
+new_precipitation_list = []
+
+
+print(len(dictr['timeSeries']))
 # itterate over the json file to but values excluding dates do pandas
 for i in range(len(dictr['timeSeries'])):
+    y = dictr['timeSeries'][i]['parameters']
+    # print(y)
+    for x in range(len(dictr['timeSeries'][i]['parameters'])):
 
-    recs = dictr['timeSeries'][i]['parameters']
-    print(recs)
-    df_values = df_values.append(recs) #  append(recs)
+        recs = dictr['timeSeries'][i]['parameters'][x]["name"]
+        value_dict = dictr['timeSeries'][i]['parameters'][x]
+        # the_value = dictr['timeSeries'][i]['parameters'][x]["values"]
+       # print(the_value)
+        #print(value_dict)
+        if recs == "t":
+            temp_list.append(value_dict)
+
+        elif recs == "msl":
+            air_pressure_list.append(value_dict)
+
+        elif recs == "pcat":
+            precipitation_list.append(value_dict)
 
 
+for z in range(len(temp_list)):
+    q = temp_list[z]["values"]
+    for item in q:
+        # print(item)
+        # print(float(item))
+        new_temp_list.append(float(item))
+
+for u in range(len(air_pressure_list)):
+    w = air_pressure_list[u]["values"]
+    for item in w:
+        # print(float(item))
+        new_air_pressure_list.append(float(item))
+
+for u in range(len(precipitation_list)):
+    w = precipitation_list[u]["values"]
+    for item in w:
+        # print(float(item))
+        new_precipitation_list.append(float(item))
+
+
+
+# gets the date
 recs2 = dictr['timeSeries']
 df_time = pd.json_normalize(recs2)
 # create dataframe with only date & time
-valid_time = df_time["validTime"]
+df_final_date = df_time["validTime"]
+
+# print(len(temp_list))
+
+df = pd.DataFrame([new_temp_list, new_air_pressure_list, new_precipitation_list])
+df = df.transpose()
+df.columns = ["temp", "air_pressure", "precipitation"]
+
+df_final = pd.merge(df_final_date, df, left_index=True, right_index=True)
+
+print(df_final)
 
 
-# Getting the temprature value from pandas
-df_temp = df_values.loc[df_values['name'] == "t"]
-# reset index for df_temp in dataframe
-df_temp.reset_index(inplace=True)
-
-# merge values with dates
-df_temp_date = pd.merge(valid_time, df_temp, left_index=True, right_index=True)
-
-# df_temp_date["values"] = df_temp_date["values"].apply(pd.to_numeric)
-# print(df_temp_date)
-# print(df_temp_date.dtypes)
-
-# df_temp_date["values"] = df_temp_date["values", [float(i) for i in df_temp_date["values"]]]
-# df_temp_date["values"].astype(float)
-print(df_temp_date)
-
-
-# print(df_temp_date)
-# get todays date for saving
 to_day = datetime.now().strftime("%Y_%m_%d_%I_%M_%S_%p")
 
-# df to json
-# df_temp_date.to_json(CURR_DIR_PATH + "/data/" + str(to_day) + "_" + ".json")
-# df_temp_date.to_csv(CURR_DIR_PATH + "/data/" + str(to_day) + "_" + ".csv")
 
-
-def get_raw():
-
-    if response.status_code == 200:  # If connection is successful (200: http ok)
-        json_data = response.json()  # Get result in json
-        # print(jason_data)
-        # Create a dictionary to represent the stored data
-        # To view all accessible data see: https://openweathermap.org/current#current_JSON
-
-        # print(df)
-        weather_list = []
-        for i in range(len(json_data['timeSeries'])):
-
-            weather_data = {
-                "temperature": json_data['timeSeries'][i]["parameters"][1]['values'][0],
-                "air pressure": json_data['timeSeries'][i]["parameters"][11]['values'][0],
-                "precipitation": json_data['timeSeries'][i]["parameters"][3]['values'][0],
-                "date": json_data['timeSeries'][i]['validTime']
-            }
-            # print(json_data['timeSeries'][i]['parameters'][10])
-            # print(json_data["timeSeries"][i]['parameters'][1]["name"])
+# df.to_csv(CURR_DIR_PATH + "/data/" + str(to_day) + "_" + ".csv", index=False)
+# df.to_json(CURR_DIR_PATH + "/data/" + str(to_day) + "_" + ".json")
 
 
 
-            # result = [new_k for new_k in my_dictionary.items() if new_k[1] == new_val]
-            # for x in json_data["timeSeries"][i]['parameters'][0]["name"]:
-                # print(x)
-             #print(json_data['timeSeries'][i]['parameters'][10])
-            # print(json_data['timeSeries'][i]['parameters'][10])
-
-
-            weather_list.append(weather_data)
-
-        df = pd.DataFrame(weather_list)
-        # print(df)
-        to_day = datetime.now().strftime("%Y_%m_%d_%I_%M_%S_%p")
-
-        # df.to_csv(CURR_DIR_PATH + "/data/" + str(to_day) + "_" + ".csv", index=False)
-        # df.to_json(CURR_DIR_PATH + "/data/" + str(to_day) + "_" + ".json")
-
-
-
-# get_raw()
 
 
 
 
 def plot_weather():
-    data = pd.read_json(CURR_DIR_PATH + "/data/" + "2022_07_13_03_38_33_PM_.json")
+    data = pd.read_json(CURR_DIR_PATH + "/data/" + "2022_07_14_10_07_55_AM_.json")
     df = pd.read_json(CURR_DIR_PATH + "/data/" + "2022_07_13_03_38_33_PM_.json")
     # print(df)
     # print(df)
@@ -131,17 +127,17 @@ def plot_weather():
     # print(index_date)
     # dates_array = np.array(days)
     # print(dates_array)
-    plt.plot(df["values"])
-    # plt.plot(data["air pressure"])
+    plt.plot(df["temp"])
+    plt.plot(df["air_pressure"])
     # plt.plot(days)
 
     # Adding Title to the Plot
     plt.title("Scatter Plot")
 
     # Setting the X and Y labels
-    plt.xlabel('hours')
-    plt.ylabel('temp')
-    # plt.show()
+    plt.xlabel('temp')
+    plt.ylabel('air_pressure')
+    plt.show()
 
-# plot_weather()
+plot_weather()
 
