e3c782b1faddbab4b3fe3ee6a6f236bb75653f1d
nscodechris <christoffer.vdmab@gmail.com>
2022-07-14 14:24:32 +0200

    dag_initiate

--- weather_new.py
+++ weather_new.py
@@ -5,27 +5,15 @@ import matplotlib.pyplot as plt
 import psycopg2
 from datetime import datetime
 from sqlalchemy import create_engine
-
+from sqlalchemy import text
+from airflow import DAG
+from airflow.operators.python import PythonOperator, BranchPythonOperator
+from airflow.operators.bash import BashOperator
 
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
-
-CURR_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-# /api/category/pmp3g/version/2/geotype/multipoint/validtime/20151012T150000Z/parameter/t/leveltype/hl/level/2/data.json?with-geo=false
-# Accept-Encoding: gzip
-url = 'https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/16.158/lat/58.5812/data.json'
-print(url)
-# r = requests.get(url)
-# json_raw = r.json()
-
-response = requests.get(url)
-dictr = response.json()
-
-# print(len(dictr['timeSeries']))
-list_values = []
-df_values = pd.json_normalize(list_values)
 
 temp_list = []
 air_pressure_list = []
@@ -36,7 +24,15 @@ new_temp_list = []
 new_air_pressure_list = []
 new_precipitation_list = []
 
-def get_values():
+
+CURR_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+url = 'https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/16.158/lat/58.5812/data.json'
+response = requests.get(url)
+dictr = response.json()
+
+
+
+def _get_values():
 
     # itterate over the json file to but values excluding dates do pandas
     for i in range(len(dictr['timeSeries'])):
@@ -78,9 +74,7 @@ def get_values():
             new_precipitation_list.append(float(item))
 
 
-
-
-def list_to_pandas():
+def _list_to_pandas():
 
     # gets the date
     recs2 = dictr['timeSeries']
@@ -97,28 +91,14 @@ def list_to_pandas():
     df_final = pd.merge(df_final_date, df, left_index=True, right_index=True)
 
     # print(df_final)
-    to_day = datetime.now().strftime("%Y_%m_%d_%I_%M_%S_%p")
-    # df.to_csv(CURR_DIR_PATH + "/data/" + str(to_day) + "_" + ".csv", index=False)
-    # df.to_json(CURR_DIR_PATH + "/data/" + str(to_day) + "_" + ".json")
+    df_final.to_csv(CURR_DIR_PATH + "/data/" + "weather.csv", index=False)
+    df_final.to_json(CURR_DIR_PATH + "/data/" + "weather.json")
     return df_final
-
 
 def plot_weather():
     data = pd.read_json(CURR_DIR_PATH + "/data/" + "2022_07_14_10_07_55_AM_.json")
     df = pd.read_json(CURR_DIR_PATH + "/data/" + "2022_07_14_10_07_55_AM_.json")
-    # print(df)
-    # print(df)
-    # print(type(data['date']))
-    # days = data["date"].dt.day
-    # days_unique = days.unique()
-    # temp_mean = data["temperature"].mean()
-    # print(days_unique)
-    # print(days)
-    # print(data["temperature"])
-    # index_date = data["date"][0]
-    # print(index_date)
-    # dates_array = np.array(days)
-    # print(dates_array)
+
     plt.plot(df["temp"])
     plt.plot(df["air_pressure"])
     # plt.plot(days)
@@ -131,14 +111,34 @@ def plot_weather():
     plt.ylabel('air_pressure')
     plt.show()
 
-
-def pandas_to_database(data_frame):
-
+def _pandas_to_database():
+    df_weather = pd.read_json(CURR_DIR_PATH + "/data/" + "weather.json")
     engine = create_engine('postgresql://postgres:Cvmillan10!?@localhost:5432/weather_group')
-    data_frame.to_sql('weathers', engine)
+    df_weather.to_sql('weathers', engine, if_exists='replace')
     # weather_group-# CREATE USER weather_user WITH PASSWORD 'weather_123";
 
-# get_values()
-# pandas_to_database(list_to_pandas())
+with DAG("weather_group_dag", start_date=datetime(2022, 1, 1),
+         schedule_interval=None, catchup=False) as dag:
+    get_values = PythonOperator(
+        task_id="get_values",
+        python_callable=_get_values
+    )
+
+    list_to_pandas = BranchPythonOperator(
+        task_id="list_to_harmonize",
+        python_callable=_list_to_pandas
+    )
+
+    pandas_to_database = BranchPythonOperator(
+        task_id="pandas_to_database",
+        python_callable=_pandas_to_database
+    )
+    get_values >> list_to_pandas >> pandas_to_database
+
+
+
+_get_values()
+_list_to_pandas()
+_pandas_to_database()
 # plot_weather()
 
