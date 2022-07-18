b939216688a27b3a2d85022fd4b4c6a77fdacedd
nscodechris <christoffer.vdmab@gmail.com>
2022-07-18 09:58:31 +0200

    final_version

--- weather_new.py
+++ weather_new.py
@@ -30,20 +30,27 @@ class WeatherClass:
         self.response = requests.get(self.url)
         self.dictr = self.response.json()
 
+    # getting all values except date
     def get_values(self):
 
-        # itterate over the json file to but values excluding dates do pandas
+        # i gets the index in the json_file from key timeSeries, the key has a dict in a list as values
         for i in range(len(self.dictr['timeSeries'])):
-            y = self.dictr['timeSeries'][i]['parameters']
-            # print(y)
+            # getting all values from the key "timeSeries"
+            # y = self.dictr['timeSeries'][i]['parameters']
+            # x gets the index of the parameters key
             for x in range(len(self.dictr['timeSeries'][i]['parameters'])):
 
+                # recs gets the values: like "hl", "t", "hmsl"
                 recs = self.dictr['timeSeries'][i]['parameters'][x]["name"]
+                # print(recs)
+                # value_dict, get the values from the key parameters
                 value_dict = self.dictr['timeSeries'][i]['parameters'][x]
+                #  print(value_dict)
                 # the_value = dictr['timeSeries'][i]['parameters'][x]["values"]
                # print(the_value)
                 #print(value_dict)
                 if recs == "t":
+                    # appending the value from the parameter "parameters"
                     self.temp_list.append(value_dict)
 
                 elif recs == "msl":
@@ -51,7 +58,7 @@ class WeatherClass:
 
                 elif recs == "pcat":
                     self.precipitation_list.append(value_dict)
-
+        # break out the values in the list from key "values" converts list to float
         for z in range(len(self.temp_list)):
             q = self.temp_list[z]["values"]
             for item in q:
@@ -77,7 +84,9 @@ class WeatherClass:
         WeatherClass.get_values(self)
         # gets the date
         recs2 = self.dictr['timeSeries']
+        # print(recs2)
         df_time = pd.json_normalize(recs2)
+        # print(df_time)
         # create dataframe with only date & time
         df_time["date"] = pd.to_datetime(df_time['validTime']).dt.strftime('%Y/%m/%d')
         df_time["hours"] = pd.to_datetime(df_time['validTime']).dt.time
@@ -87,16 +96,13 @@ class WeatherClass:
         df_final_date = df_time[["date", "hours"]].copy()
         # print(df_final_date)
 
-        # print(df_time['validTime'])
-        # print(df_final_date)
-        # print(df_final_date.dtypes)
-
-        # print(len(temp_list))
-
-        # concate list to
+        # creating new data_frame with values, from lists
         df = pd.DataFrame([self.new_temp_list, self.new_air_pressure_list, self.new_precipitation_list])
+        # print(df)
         df = df.transpose()
+        # print(df)
         df.columns = ["temp", "air_pressure", "precipitation"]
+        # print(df)
         df_final = pd.merge(df_final_date, df, left_index=True, right_index=True)
 
         # print(df_final)
@@ -109,11 +115,16 @@ class WeatherClass:
         df_weather = pd.read_json(self.CURR_DIR_PATH + "/data/" + "weather.json")
         engine = create_engine('postgresql://postgres:1234@localhost:5432/weather_group')
         df_weather.to_sql('weathers', engine, if_exists='replace')
+        with engine.connect() as connection:
+            connection.execute("ALTER TABLE weathers ALTER COLUMN date TYPE date;")
         # weather_group-# CREATE USER weather_user WITH PASSWORD 'weather_123";
 
 
 smhi = WeatherClass()
+# print(smhi.dictr)
+# smhi.get_values()
 # smhi.list_to_pandas()
+# smhi.pandas_to_database()
 
 
 with DAG("weather_group_dag", start_date=datetime(2022, 1, 1),
